01c3703d8dc9a8a105d3f4018d34acdbd8ff8fac
Kohei Suzuki <jingle@sfc.wide.ad.jp>
2018-02-13 00:48:01 +0900

    add TestDB Handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Person struct {
@@ -19,21 +21,55 @@ type Person struct {
 	Address   *Address `json:"address,omitempty"`
 }
 
-type App struct {
-	Router *mux.Router
-	// DB     *sql.DB
-}
-
 type Address struct {
 	City  string `json:"city,omitempty"`
 	State string `json:"state,omitempty"`
+}
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
 }
 
 var people []Person
 
 func (a *App) Initialize() {
 	a.Router = mux.NewRouter()
+	a.InitializeDB()
 	a.initializeRoutes()
+}
+
+func (a *App) InitializeDB() {
+	var err error
+
+	os.Remove(os.Getenv("DBFILE"))
+	a.DB, err = sql.Open("sqlite3", os.Getenv("DBFILE"))
+	if err != nil {
+		log.Fatal("sql.Open:", err)
+	}
+
+	_, err = a.DB.Exec(`CREATE TABLE IF NOT EXISTS "people" (
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"firstname" VARCHAR(255),
+			"lastname" VARCHAR(255),
+			"city" VARCHAR(255),
+			"state" VARCHAR(255))`)
+
+	if err != nil {
+		log.Fatal("CREATE TABLE: ", err)
+	}
+
+	_, err = a.DB.Exec(
+		`INSERT INTO "people" ("firstname", "lastname", "city", "state") VALUES(?, ?, ?, ?)`,
+		"Taro",
+		"Hoge",
+		"Fujisawa",
+		"Kanagawa",
+	)
+
+	if err != nil {
+		log.Fatal("INSERT INTO: ", err)
+	}
 }
 
 func (a *App) initializeRoutes() {
@@ -41,6 +77,7 @@ func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/people/{id}", a.GetPersonEndpoint).Methods("GET")
 	a.Router.HandleFunc("/people/{id}", a.CreatePersonEndpoint).Methods("POST")
 	a.Router.HandleFunc("/people/{id}", a.DeletePersonEndpoint).Methods("DELETE")
+	a.Router.HandleFunc("/testdb", a.TestDB).Methods("GET")
 }
 
 func (a *App) Run(addr string) {
@@ -103,4 +140,28 @@ func (a *App) DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode(people)
+}
+
+func (a *App) TestDB(w http.ResponseWriter, r *http.Request) {
+	results := []Person{}
+
+	rows, err := a.DB.Query(`SELECT * from people`)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var p Person
+		var a Address
+		err = rows.Scan(&p.ID, &p.Firstname, &p.Lastname, &a.City, &a.State)
+		if err != nil {
+			log.Fatal(err)
+		}
+		p.Address = &a
+
+		results = append(results, p)
+	}
+
+	json.NewEncoder(w).Encode(results)
 }
